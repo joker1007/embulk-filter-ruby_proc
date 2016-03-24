529857922aba6d76ee3e1deefec4bfe37db424cb
joker1007 <kakyoin.hierophant@gmail.com>
2016-03-24 17:29:40 +0900

    Implement rows proc feature

--- lib/embulk/filter/ruby_proc.rb
+++ lib/embulk/filter/ruby_proc.rb
@@ -6,7 +6,8 @@ module Embulk
 
       def self.transaction(config, in_schema, &control)
         task = {
-          "columns" => config.param("columns", :array),
+          "columns" => config.param("columns", :array, default: []),
+          "rows" => config.param("rows", :array, default: []),
           "requires" => config.param("requires", :array, default: []),
         }
 
@@ -27,6 +28,7 @@ module Embulk
         task["requires"].each do |lib|
           require lib
         end
+
         @procs = Hash[task["columns"].map {|col|
           if col["proc"]
             [col["name"], eval(col["proc"])]
@@ -34,6 +36,15 @@ module Embulk
             [col["name"], eval(File.read(col["proc_file"]), binding, File.expand_path(col["proc_file"]))]
           end
         }]
+        @row_procs = task["rows"].map {|rowdef|
+          if rowdef["proc"]
+            eval(rowdef["proc"])
+          else
+            eval(File.read(rowdef["proc_file"]), binding, File.expand_path(rowdef["proc_file"]))
+          end
+        }.compact
+        raise "Need columns or rows parameter" if @row_procs.empty? && @procs.empty?
+
         @skip_nils = Hash[task["columns"].map {|col|
           [col["name"], col["skip_nil"].nil? ? true : !!col["skip_nil"]]
         }]
@@ -44,18 +55,27 @@ module Embulk
 
       def add(page)
         page.each do |record|
-          record_hash = hashrize(record)
-          @procs.each do |col, pr|
-            next unless record_hash.has_key?(col)
-            next if record_hash[col].nil? && @skip_nils[col]
-
-            if pr.arity == 1
-              record_hash[col] = pr.call(record_hash[col])
-            else
-              record_hash[col] = pr.call(record_hash[col], record_hash)
+          if @row_procs.empty?
+            record_hashes = [hashrize(record)]
+          else
+            record_hashes = @row_procs.flat_map do |pr|
+              pr.call(hashrize(record))
             end
           end
-          page_builder.add(record_hash.values)
+
+          record_hashes.each do |record_hash|
+            @procs.each do |col, pr|
+              next unless record_hash.has_key?(col)
+              next if record_hash[col].nil? && @skip_nils[col]
+
+              if pr.arity == 1
+                record_hash[col] = pr.call(record_hash[col])
+              else
+                record_hash[col] = pr.call(record_hash[col], record_hash)
+              end
+            end
+            page_builder.add(record_hash.values)
+          end
         end
       end
 
